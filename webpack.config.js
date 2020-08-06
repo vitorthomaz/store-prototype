62240aab4197c2bec066ed4756615618a9f004d2
Vitor Thomaz <vthomaz1996@gmail.com>
2020-08-05 21:07:20 -0300

    feat: Created webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    port: 3000
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  entry: [path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+      favicon: path.join(__dirname, 'public', 'favicon.ico')
+    })
+  ]
+};
