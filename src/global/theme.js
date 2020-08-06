2ed1775409fe680b90ab36b55cdd761572d1b683
Vitor Thomaz <vthomaz1996@gmail.com>
2020-08-06 19:18:35 -0300

    feat: Added global theme with styled-components

---
+++ src/global/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    yellow: '#F9C22E',
+    black: '#0C090D',
+    white: '#F8E9E9',
+    green: '#8FC0A9',
+    blue: '#68B0AB'
+  },
+  cards: {
+    green: '#4F772D',
+    red: '#B80C09'
+  }
+};
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
 
 import { ErrorBoundary } from './components';
 
+import { theme } from './global';
+
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
