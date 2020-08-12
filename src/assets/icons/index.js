832536a103d40d6e8f48140516a3eac673a2dfd7
Vitor Thomaz <vthomaz1996@gmail.com>
2020-08-11 22:38:55 -0300

    refactor: Removed back to top button

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,8 @@
 export { default as Github } from './github';
 export { default as Logo } from './logo';
-export { default as ArrowUp } from './arrow_up';
 export { default as Linkedin } from './linkedin';
 export { default as Search } from './search';
 export { default as Add } from './add';
 export { default as Remove } from './remove';
 export { default as Shopping } from './shopping';
+export { default as Home } from './home';
--- src/components/Footer/components/index.js
+++ src/components/Footer/components/index.js
@@ -1,2 +1,1 @@
 export { default as BottomNav } from './BottomNav';
-export { default as BackToTop } from './BackToTop';
