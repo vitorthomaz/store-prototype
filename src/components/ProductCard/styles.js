476fa125c28aa89a956c9147d535726fcd153bff
Vitor Thomaz <vthomaz1996@gmail.com>
2020-08-11 22:46:09 -0300

    refactor: Added padding inside cards

--- src/components/ProductCard/styles.js
+++ src/components/ProductCard/styles.js
@@ -1,17 +1,29 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const appear = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.3);
+  }
+
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
 
 export const Card = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-
-  width: 100%;
-  height: 100%;
+  padding: 16px;
 
   background: #fafafa;
   border-radius: 6px;
   transition: all 500ms;
+  animation: ${appear} 1s;
+  animation-fill-mode: backwards;
 
   :hover {
     transform: scale(1.02);
@@ -32,7 +44,9 @@ export const Title = styled.h1`
   font-size: 12px;
 `;
 
-export const Price = styled.h2``;
+export const Price = styled.h2`
+  margin: 4px 0;
+`;
 
 export const CartZone = styled.div`
   display: flex;
